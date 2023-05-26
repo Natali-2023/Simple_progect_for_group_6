e2ec59f64162d28591cc830f584613468fe82096
Nataliia <pnata1978@gmail.com>
2023-05-26 13:40:10 +0200

    add corrected1

--- src/main/java/de/ait/repositories/UsersRepository.java
+++ src/main/java/de/ait/repositories/UsersRepository.java
@@ -7,7 +7,5 @@ import java.util.List;
 public interface UsersRepository {
     List<User> findAll();
 
-
-    public void addPerson(String x);
-
+    public void addPerson(String x); // новая реализация сканнера через main
     }
